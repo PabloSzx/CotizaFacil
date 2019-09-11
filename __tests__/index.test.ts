02bdbeb165b9582a5f747006f90612f3a90e174c
PabloSz <pablosaez1995@gmail.com>
2019-09-11 11:50:18 -0300

    testing

---
+++ __tests__/index.test.ts
@@ -0,0 +1,5 @@
+test("should sum", () => {
+  expect(1 + 1).toBe(2);
+});
+
+export default () => {};
---
+++ src/server/entities/User.ts
@@ -0,0 +1,17 @@
+import { Field, ObjectType } from "type-graphql";
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity()
+@ObjectType()
+export class User {
+  @PrimaryColumn()
+  @Field()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Field()
+  @Column()
+  name: string;
+}
